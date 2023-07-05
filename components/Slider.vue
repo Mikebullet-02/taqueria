66b159781164761ebb656270572dd1f1735c3154
Mikebullet-02 <72115697+Mikebullet-02@users.noreply.github.com>
2023-07-04 18:22:07 -0600

    Add files via upload

---
+++ components/Slider.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="flex flex-col md:flex-row">
+    <div class="w-full md:w-1/2">
+      <div class="carousel">
+        <img
+          v-for="(item, index) in images"
+          :key="index"
+          :src="item.image"
+          :alt="item.description"
+          :class="{ active: index === currentIndex }"
+          class="carousel-image"
+        />
+      </div>
+    </div>
+    <div class="w-full md:w-1/2">
+      <div class="carousel-description">
+        <h3 class="text-2xl font-bold mb-4">{{ currentImage.description }}</h3>
+        <p>{{ currentImage.caption }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+      images: [
+        {
+          image: "/src/assets/Toy_Fest/Taqueria/IMG_2006.jpg",
+          description: "Toy Fest",
+          caption: "14 de mayo de 2023",
+        },
+        {
+          image: "/src/assets/Toy_Fest/Taqueria/IMG_2269.jpg",
+          description: "Collabs",
+          caption: "interesantes",
+        },
+        {
+          image: "/src/assets/Toy_Fest/Taqueria/IMG_2005.jpg",
+          description: "La mercancía",
+          caption: "de ElFrittangas13",
+        },
+        {
+          image: "/src/assets/Toy_Fest/Taqueria/IMG_2009.jpg",
+          description: "La comida",
+          caption: "de La Fábrica de Dulce",
+        },
+      ],
+    };
+  },
+  computed: {
+    currentImage() {
+      return this.images[this.currentIndex];
+    },
+  },
+  mounted() {
+    setInterval(this.nextImage, 3000);
+  },
+  methods: {
+    nextImage() {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.carousel {
+  position: relative;
+  height: 0;
+  padding-top: 56.25%; /* 16:9 aspect ratio */
+}
+
+.carousel-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+
+.carousel-image.active {
+  opacity: 1;
+}
+
+.carousel-description {
+  padding: 1rem;
+  text-align: center;
+}
+</style>
