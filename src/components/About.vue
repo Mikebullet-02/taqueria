8780feb1446ff44ead638355d84b499808ceb172
Mikebullet-02 <72115697+Mikebullet-02@users.noreply.github.com>
2023-07-10 18:05:13 -0600

    Add files via upload

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,56 +1,101 @@
 <template>
-  <section class="bg-black">
-    <div class="container mx-auto">
-      <h2 class="text-3xl font-semibold mb-6 pl-5 pt-8 text-red-600">
-        Acerca de nosotros
-      </h2>
-      <div class="flex flex-wrap">
-        <div class="w-full md:w-1/2 lg:w-1/3 p-4">
+  <div class="container px-4 bg-black border border-gray-800 w-full">
+    <h2 class="text-3xl font-bold text-center mb-6 text-red-800">
+      Un poco de nosotros
+    </h2>
+    <div class="relative pb-10">
+      <div class="carousel-container aspect-w-16 aspect-h-9 pb-10 w-full">
+        <div class="carousel-track">
           <div
-            class="bg-gray-800 rounded-lg shadow-md p-6 border border-gray-600"
+            v-for="(video, index) in videos"
+            :key="index"
+            class="carousel-slide aspect-w-16 aspect-h-9 transition-opacity"
+            :class="{ 'carousel-active': index === currentIndex }"
           >
-            <h3 class="text-xl font-semibold mb-4 text-gray-100">
-              Nuestra Historia
-            </h3>
-            <p class="text-gray-200">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              vehicula sem ac risus dapibus rhoncus.
-            </p>
-          </div>
-        </div>
-        <div class="w-full md:w-1/2 lg:w-1/3 p-4">
-          <div
-            class="bg-gray-800 rounded-lg shadow-md p-6 border border-gray-600"
-          >
-            <h3 class="text-xl font-semibold mb-4 text-gray-100">
-              Nuestra Misión
-            </h3>
-            <p class="text-gray-200">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-              pharetra dolor eu mauris pulvinar, id finibus lectus dapibus.
-            </p>
-          </div>
-        </div>
-        <div class="w-full md:w-1/2 lg:w-1/3 p-4">
-          <div
-            class="bg-gray-800 rounded-lg shadow-md p-6 border border-gray-600"
-          >
-            <h3 class="text-xl font-semibold mb-4 text-gray-100">
-              Nuestro Equipo
-            </h3>
-            <p class="text-gray-200">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              vestibulum eros et risus malesuada, in euismod metus dignissim.
-            </p>
+            <iframe
+              :src="'https://www.youtube.com/embed/' + video.videoId"
+              frameborder="0"
+              allowfullscreen
+              class="w-full h-96"
+            ></iframe>
           </div>
         </div>
       </div>
+      <div class="carousel-nav mt-0 flex justify-between">
+        <button
+          class="carousel-prev px-6 py-4 bg-red-800 text-white font-bold rounded disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="prevSlide"
+        >
+          Anterior
+        </button>
+        <button
+          class="carousel-next px-6 py-4 bg-red-800 text-white font-bold rounded disabled:opacity-50 disabled:cursor-not-allowed"
+          @click="nextSlide"
+        >
+          Siguiente
+        </button>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
+
+<style>
+.carousel-slide {
+  display: none;
+  opacity: 0;
+}
+
+.carousel-active {
+  display: block;
+  opacity: 1;
+  transition-duration: 0.3s;
+}
+
+.carousel-prev,
+.carousel-next {
+  transition: opacity 0.3s;
+}
+
+.carousel-prev:hover,
+.carousel-next:hover {
+  opacity: 0.8;
+}
+</style>
 
 <script>
 export default {
-  name: "AboutUsSection",
+  name: "YouTubeCarousel",
+  data() {
+    return {
+      videos: [
+        { videoId: "Qi-atfC1CWk" },
+        { videoId: "xPixWV8T4lY" },
+        { videoId: "sdT_rFNHHmA" },
+        // Add more videos as needed
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    currentVideo() {
+      return this.videos[this.currentIndex];
+    },
+  },
+  methods: {
+    nextSlide() {
+      if (this.currentIndex === this.videos.length - 1) {
+        this.currentIndex = 0;
+      } else {
+        this.currentIndex++;
+      }
+    },
+    prevSlide() {
+      if (this.currentIndex === 0) {
+        this.currentIndex = this.videos.length - 1;
+      } else {
+        this.currentIndex--;
+      }
+    },
+  },
 };
 </script>
